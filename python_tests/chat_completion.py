631e1c2d79240591c4672821328e59a48493256f
Joao Rebelo <jrebelo@s2e-systems.com>
2025-07-28 11:56:45 +0200

    Move chat completion test

--- python_tests/chat_completion.py
+++ python_tests/chat_completion.py
@@ -2,7 +2,7 @@ import os
 from openai import OpenAI
 client = OpenAI(
     base_url="http://localhost:3456/v1",
-    api_key=os.environ.get("OPENAI_API_KEY"),  # This is the default and can be omitted
+    # api_key=os.environ.get("OPENAI_API_KEY"),  # This is the default and can be omitted
 )
 chat_completion = client.chat.completions.create(
     model="gpt-4o",
